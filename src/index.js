cf35b9e88eac5056d7e8762e6c3782a0cbea91f9
Josh DeVore <jrd1185@gmail.com>
2024-09-21 17:35:25 -0600

    Initial Project upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css'; // Make sure this path matches your actual file location
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
